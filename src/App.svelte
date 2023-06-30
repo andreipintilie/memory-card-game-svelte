d8b5dee836bd19a64d5b8bdb19cce03945e3c189
Andrei Pintilie <andrei.pintilie00@e-uvt.ro>
2023-07-01 01:31:17 +0300

    Updated score board

--- src/App.svelte
+++ src/App.svelte
@@ -21,7 +21,7 @@
     };
 
     let possibleMatches = 6;
-    const options = [2, 6, 9, 13, 16];
+    const options = [6, 9, 13, 16];
 
     const handleSelectChange = (e) => {
       possibleMatches = parseInt(e.target.value);
@@ -50,42 +50,43 @@
 </script>
 
 <main class={`${className} ${classNameSecondary}`}>
-    {#if !playing}
-        <SvelteLogo />
-        <h1>Memory Card Game</h1>
-        <button class="menu-button mb-4" on:click={toggleGameStatus}>Play</button>
-        <button class="menu-button" on:click={openScoreBoard}>Score</button>
-        <small>Maximum matches:</small>
-        <div class="dropdown">
-          <select on:change={handleSelectChange}>
-            {#each options as option}
-              <option value={option} selected={option === possibleMatches}>{option}</option>
+  {#if !playing}
+      <SvelteLogo />
+      <h1>Memory Card Game</h1>
+      <button class="menu-button mb-4" on:click={toggleGameStatus}>Play</button>
+      <button class="menu-button" on:click={openScoreBoard}>Score</button>
+      <small>Maximum matches:</small>
+      <div class="dropdown">
+        <select on:change={handleSelectChange}>
+          {#each options as option}
+            <option value={option} selected={option === possibleMatches}>{option}</option>
+          {/each}
+        </select>
+        <Fa icon={faChevronDown} />
+      </div>
+      <p class="createdWith">Created with Svelte.js</p>
+
+      {#if showScoreBoard}
+        <Modal class="score-board" onClickOutside={closeScoreBoard}>
+          <h2 class="title">Score History</h2>
+          <div class="wrapper overflow-y-auto">
+            <p class="pretitle">Difficulty</p>
+            <p class="pretitle">Attempts</p>
+            <p class="pretitle">Time</p>
+            {#each scores as score}
+              <p>{score.difficulty}</p>
+              <p>{score.attempts}</p>
+              <p>{score.time}</p>
             {/each}
-          </select>
-          <Fa icon={faChevronDown} />
-        </div>
-        <p class="createdWith">Created with Svelte.js</p>
+          </div>
+          <button class="btn" on:click={() => {
+            scores = []
+            localStorage.removeItem('memorycardgame')
+          }}>Clear Score List</button>
+        </Modal>
+      {/if}
 
-        {#if showScoreBoard}
-          <Modal class="score-board" onClickOutside={closeScoreBoard}>
-            <h2 class="title">Top Score</h2>
-            <div class="wrapper overflow-y-auto">
-              {#each scores as score}
-                <div class="inline">
-                  <p>Difficulty: {score.difficulty}</p>
-                  <p>Attempts: {score.attempts}</p>
-                  <p>Time: {score.time}</p>
-                </div>
-              {/each}
-            </div>
-            <button class="btn" on:click={() => {
-              scores = []
-              localStorage.removeItem('memorycardgame')
-            }}>Clear Score List</button>
-          </Modal>
-        {/if}
-
-    {:else}
-        <Game on:gameOver={toggleGameStatus} {possibleMatches} />
-    {/if}
+  {:else}
+      <Game on:gameOver={toggleGameStatus} {possibleMatches} />
+  {/if}
 </main>
